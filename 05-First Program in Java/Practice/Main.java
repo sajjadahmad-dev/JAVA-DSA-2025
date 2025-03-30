ce74ac61c266592250abfd35c37461c5ce6e6ff3
sajjadahmad-dev <sajjadahmad.code@gmail.com>
2025-03-31 01:17:39 +0500

    solve error

--- 05-First Program in Java/Practice/Main.java
+++ 05-First Program in Java/Practice/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
         
     }{
-        System.out.println(args[0]);
+        System.out.println("hello world");
+    
 
 
     } 
