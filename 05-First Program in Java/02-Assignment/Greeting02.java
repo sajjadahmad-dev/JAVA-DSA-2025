3de85e0424466044412f6c5d8caf22ccdc9f10a8
sajjadahmad-dev <sajjadahmad.code@gmail.com>
2025-04-02 23:08:28 +0500

    change code

--- 05-First Program in Java/02-Assignment/Greeting02.java
+++ 05-First Program in Java/02-Assignment/Greeting02.java
@@ -7,6 +7,6 @@ public class Greeting02 {
     public static void main(String[] args) {
         System.out.println("Enter your name: ");
         Scanner input = new Scanner(System.in);
-        System.out.println(input.nextLine() + " you are doing great! keep it up! you'll have to prove your self. Realise your opponents they have lost realy a diamond!");
+        System.out.println(input.nextLine() + " you are doing great! keep it up! you'll have to prove your self thanks. Realise your opponents they have lost realy a diamond!");
     }
 }
