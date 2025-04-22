f585741a623889a8fed02306cc13914a8cd2bbc7
sajjadahmad-dev <sajjadahmad.code@gmail.com>
2025-04-22 17:41:44 +0500

    linear search in 2D array

---
+++ 09-Linear Search/code/FindMaxIn2d.java
@@ -0,0 +1,31 @@
+public class FindMaxIn2d {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {23, 4, 1},
+                {18, 12, 3, 9},
+                {78, 99, 34, 56},
+                {18, 12}
+        };
+
+        System.out.println(Max(arr));
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+    static int Max(int[][] arr) {
+        int max= Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] > max) {
+                    max= arr[row][col];
+                }
+            }
+             
+        }
+        return max;
+        
+    }
+
+    
+}
--- 09-Linear Search/code/SearchIn2D.java
+++ 09-Linear Search/code/SearchIn2D.java
@@ -1,13 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class SearchIn2D {
     public static void main(String[] args) {
-        int [][] arr ={
-            {1,2,3,4},
-            {5,6,7,8},
-            {9,0,1,2},
-            {8,3,4,2}
-                };
-            
-        
+        int[][] arr = {
+                {23, 4, 1},
+                {18, 12, 3, 9},
+                {78, 99, 34, 56},
+                {18, 12}
+        };
+        int target = 96;
+        int[] ans = search(arr,target); // format of return value {row, col}
+        System.out.println(Arrays.toString(ans));
+
     }
 
-}
+    static int[] search(int[][] arr, int target) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    }
