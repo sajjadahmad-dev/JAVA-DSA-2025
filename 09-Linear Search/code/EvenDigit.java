ba7e970ee6ada829cb9f13e80f75ca3a626edf8b
sajjadahmad-dev <sajjadahmad.code@gmail.com>
2025-04-23 18:35:17 +0500

    Q

---
+++ 09-Linear Search/code/EvenDigit.java
@@ -0,0 +1,28 @@
+public class EvenDigit {
+    public static void main(String[] args) {
+        int[] nums = {12, 345, 24, 6, 7896};  // Array of numbers
+        System.out.println(findNumbers(nums)); // Print the result
+    }
+
+    // Function to find how many numbers have even digits
+    public static int findNumbers(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            int digits = countDigits(num);  // Count the digits
+            if (digits % 2 == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    // Function to count digits in a number
+    public static int countDigits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count;
+    }
+}
